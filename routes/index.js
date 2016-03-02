6dff84f11b6576c28b4f792643f3f064205fa08e
Vlad Paskar <vlad.paskarus@gmail.com>
2016-03-02 06:28:57 -0800

    Add room joiner request handler and form inputs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/join', function(req, res, next) {
+  var roomId = req.query.room;
+  console.log(roomId);
+  res.render('index', { title: 'Room ' + roomId });
+});
+
 module.exports = router;
