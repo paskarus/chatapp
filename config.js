b574daba6476ecf113c0cd287b065828f294be9a
Vlad Paskar <vlad.paskarus@gmail.com>
2016-03-04 10:35:27 -0800

    Add MONGOLAB_URI to configs

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // TODO: migrate to env variables instead of hardcoded values
 var config = {
-    mongodb_url: "mongodb://localhost:27017/chat",
+    mongodb_url: process.env.MONGOLAB_URI || "mongodb://localhost:27017/chat",
     messages_limit: 10
 };
 
