fa586397f58e738bc89dd84077e0b1e9351659e7
Vlad Paskar <vlad.paskarus@gmail.com>
2016-03-04 10:38:05 -0800

    Remove custom logger and temporary migrate to console

--- logger.js
+++ logger.js
@@ -1,8 +1,3 @@
-const Console = require('console').Console;
-const fs = require('fs');
-const output = fs.createWriteStream('./log/stdout.log');
-const errorOutput = fs.createWriteStream('./log/stderr.log');
+"use strict";
 
-const logger = new Console(output, errorOutput);
-
-module.exports = logger;
+module.exports = console;
